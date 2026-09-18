9b84fa1fbbac01fefaa47016a9a497647c0f7869
Arinzechukwu  Nwosu <arinzechukwuf.nwosu@gmail.com>
2026-09-18 16:10:28 +0100

    Create script.js

---
+++ trivia-bot/js/script.js
@@ -0,0 +1,13 @@
+console.log("Hello! I'm your coding fun fact guide!");
+const botName = "BotCoder";
+const botLocation = "iCloud";
+const favoriteLanguage = "JavaScript";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact = "A fun fact is that " + favoriteLanguage + " powers the present web as we know it."
+console.log(codingFact);
+codingFact = "It is worthy of note to state that " + favoriteLanguage + " is popularly used for web development.";
+console.log(codingFact);
+codingFact = "Learning " + favoriteLanguage + " opens up many opportunites for you.";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
